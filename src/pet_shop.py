f5c6b5ce6d9148e655173c2f8557ddcc6553229b
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-11 22:00:53 +0100

    added add_or_remove_cash fn

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -6,3 +6,6 @@ def get_total_cash(pet_shop_dict):
     return pet_shop_dict["admin"]["total_cash"]
 
 
+def add_or_remove_cash(pet_shop_dict, cash_change_amount):
+    pet_shop_dict["admin"]["total_cash"] = get_total_cash(pet_shop_dict) + cash_change_amount
+    return pet_shop_dict
