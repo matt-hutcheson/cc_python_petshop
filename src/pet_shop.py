a36849bc77de76eb7aaaf1f10431ed41d2281a74
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-12 09:35:47 +0100

    added get_pets_sold fn

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -9,3 +9,9 @@ def get_total_cash(pet_shop_dict):
 def add_or_remove_cash(pet_shop_dict, cash_change_amount):
     pet_shop_dict["admin"]["total_cash"] = get_total_cash(pet_shop_dict) + cash_change_amount
     return pet_shop_dict
+
+
+def get_pets_sold(pet_shop_dict):
+    return pet_shop_dict["admin"]["pets_sold"]
+
+
